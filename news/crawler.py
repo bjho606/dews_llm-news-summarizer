1ad0620e56b725d373b9794e6b940ecfed7153dc
S4NGP1L <tksvlf123@gmail.com>
2025-01-03 22:11:24 +0900

    chore: install requests

--- news/crawler.py
+++ news/crawler.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import requests
+
 class PortalNews:
 
     def __init__(self, base_url: str, *args: str) -> None:
@@ -21,11 +24,34 @@ news_dict: dict[str, PortalNews] = {
 }
 
 
-def crawl(portal_name: str, *categories: str) -> None:
+def get_headline_news(category: str, portal: PortalNews) -> Optional[list]:
+    base_url: str = portal.base_url
+    path_dict: dict[str, str] = portal.category_to_path
+    if category not in path_dict:
+        return None
+    result = []
+    url = base_url + path_dict[category]
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"failed to retrieve the page from url={url}")
+        return None
+    # parse headline articles from response.text and append (article_title, article_url) to result for each article
+    return result
+
+
+def crawl(portal_name: str, *categories: str) -> dict[str, list]:
     if portal_name not in news_dict:
         raise Exception("no such portal_name: {}".format(portal_name))
-    path_dict: dict[str, str] = news_dict[portal_name]
-    result = []
+    portal: PortalNews = news_dict[portal_name]
+    result: dict[str, list] = {}
+    for category in categories:
+        print(category)
+        headline_news = get_headline_news(category, portal)
+        if headline_news is None:
+            continue
+        # from article_url, get article_body and append to result
+    return result
+
 
 def main():
     portal_name = "naver"
