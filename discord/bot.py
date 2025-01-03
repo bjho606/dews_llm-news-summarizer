63d87bf51a47f8f1a8b80af837ed2261e873bbc9
ranccat <wogurjo98@gmail.com>
2025-01-03 21:21:33 +0900

    feat: bot api request added
    - requests news to FastAPI

--- discord/bot.py
+++ discord/bot.py
@@ -1,20 +1,22 @@
-import discord
-## import requests
 from discord.ext import commands
 from dotenv import load_dotenv
+import discord
+import requests
 import os
+
 
 # load .env
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-## FASTAPI_URL = os.getenv("FASTAPI_URL")
+URL = os.getenv("BASE_URL")
 
-# create a bot instance
+# set intents
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
 intents.message_content = True
 
+# create a bot instance
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 
@@ -30,10 +32,33 @@ async def ping(ctx):
     await ctx.send("Pong!")
 
 
-# Command: News {int}
+# Command: News {category} {count}
 @bot.command()
-async def news(ctx, num: int):
-    await ctx.send(f"Top {num} news here!")
+async def news(ctx, category: str = None, count: int = 5):
+    try:
+        params = {}
+        if category:
+            params["category"] = category
+        if count:
+            params["count"] = count
+
+        response = requests.get(URL, params=params)
+        response.raise_for_status()
+        news_list = response.json()
+
+        if not news_list:
+            await ctx.send("Sorry, couldn't find any news")
+        else:
+            for n in news_list:
+                await ctx.send(format_news(n))
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+def format_news(news_str):
+    formatted_news = []
+    return formatted_news
 
 
 bot.run(TOKEN)
