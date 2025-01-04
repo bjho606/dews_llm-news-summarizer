a720e15a00a5464a29636ddfbf300fb28b051c46
ranccat <wogurjo98@gmail.com>
2025-01-04 16:36:03 +0900

    fix: default "/news" command error
    - now the command is available again

--- discord/bot.py
+++ discord/bot.py
@@ -53,22 +53,27 @@ async def ping(ctx):
 @bot.command()
 async def news(ctx, command: str = None, limit: int = 5):
     try:
-        category = parse_command(command)
+        # case 1: invalid command
+        category = "ALL"
+        if command:
+            category = parse_command(command)
+            if category == "INVALID":
+                await ctx.send("Invalid command. Please try again.")
+                return
 
-        if category == "invalid":
-            await ctx.send("Invalid command. Please try again.")
-            return
-
+        # case 2: number of news out of range
         if limit < 1 or limit > 5:
             await ctx.send("Please enter a number between 1 and 5")
             return
 
+        # set parameters for API
         params = {}
-        if category:
+        if category != "ALL":
             params["category"] = category
         if limit:
             params["limit"] = limit
 
+        # send request and get response
         response = requests.get(URL, params=params)
         response.raise_for_status()
         news_list = response.json()
@@ -82,10 +87,11 @@ async def news(ctx, command: str = None, limit: int = 5):
         print(e)
 
 
+# make actual bot message based on response
 def format_news(news_list, category, limit):
     formatted_news = ""
 
-    if category:
+    if category != "ALL":
         if len(news_list) < limit:
             formatted_news += f"**Sorry, we only found {len(news_list)} news**\n"
         formatted_news += f"**Here are the top {len(news_list)} news about {category}!**\n"
@@ -99,10 +105,12 @@ def format_news(news_list, category, limit):
     return formatted_news
 
 
+# parse short command into actual category
+# invalid category command is returned INVALID
 def parse_command(command):
     parsed_command = command_map.get(command)
     if not parsed_command:
-        return "invalid"
+        return "INVALID"
     return parsed_command
 
 
